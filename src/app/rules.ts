59d9a20fdd30ee84e0f31e9cefab8832cb8d22c2
Marcelo Fernandes <marcelo.fernandes@dce.ufb.br>
2017-07-31 13:58:37 -0300

    Task 11 finished

--- src/app/rules.ts
+++ src/app/rules.ts
@@ -5,9 +5,12 @@ import {
     EntityDetailsComponent, ShowLineComponent,
     ListingTableComponent
 } from 'angularm';
+import { TextareaFormLineComponent } from "app/widgets/textarea.formline";
 
 export let defineRules = (angularm: AngularmService) => {
     angularm
+        .ptr('form_line', '*', 'description', null, TextareaFormLineComponent, { rows: '8', cols: '50' })
+        .ptr('form_line',  '*', 'summary', null, TextareaFormLineComponent, { rows: '5', cols: '50' })
         .dptr('form_line', FormLineComponent, { inputType: 'text' })
         .dpr('show_line', ShowLineComponent)
         .detr('list_entities', ListingTableComponent)
